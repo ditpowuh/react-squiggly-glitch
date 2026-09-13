62462511556182bafe83ae946c0fc031ecbf811d
ditpowuh <helloditpowuh@gmail.com>
2026-09-14 02:27:19 +1000

    Added two SquigglyMorphGlitch presets

--- lib/constants/presets/squigglymorphglitch.ts
+++ lib/constants/presets/squigglymorphglitch.ts
@@ -7,3 +7,19 @@ export const DEFAULT_PRESET: SquigglyMorphGlitchProps = {
   layers: 1,
   noiseScale: [5, 15]
 };
+
+export const WATER_PRESET: SquigglyMorphGlitchProps = {
+  rate: 5000,
+  mode: "smooth",
+  frequency: 0.025,
+  layers: 2,
+  noiseScale: [20, 30]
+};
+
+export const BLUR_PRESET: SquigglyMorphGlitchProps = {
+  rate: 5000,
+  mode: "smooth",
+  frequency: 0.75,
+  layers: 2,
+  noiseScale: [20, 100]
+};
